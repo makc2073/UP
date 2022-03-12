adf0b9df37b4932f1b4bd121f6f50fe3b61e8c52
Ромашов Максим Сергеевич гр. ИС(ПРО)-41 <is12323@stud.KHPK.RU>
2022-03-12 13:29:56 +0700

    first commit

---
+++ src/main/java/com/example/up/DatabaseFragment.java
@@ -0,0 +1,28 @@
+package com.example.up;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DatabaseFragment extends Fragment {
+
+    public DatabaseFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_database, container, false);
+    }
+}
